0b0ca8ac68658392787ea1996748d3dfe9bced5c
muharrem gem <muharremgem28@gmail.com>
2022-08-22 15:02:14 +0300

    commit

---
+++ js_session2/app.js
@@ -0,0 +1,67 @@
+console.log('App js Running')
+
+// var a =5;
+// console.log(a);
+
+// {
+//      a = 3;
+//     console.log(a);
+// }
+// var a = 5;
+// console.log(a);
+
+// console.log(typeof 0)
+// console.log(typeof 3,14)
+// console.log(typeof "hello")
+// console.log(typeof (8>10))
+// console.log(typeof 8>10)
+// console.log(typeof '')
+
+// let b = +prompt('Birşey Giriniz')
+
+// console.log(b, typeof b)
+
+// console.log(typeof typeof 42)
+
+
+
+// let age;
+
+// console.log(age)
+
+// console.log(myAge)
+
+// var myAge = 40;
+
+// console.log("hello")
+
+// let a = null
+// console.log(typeof a)
+
+// let x ;
+
+// do {
+//     x = prompt ("enter a number :");
+
+// }while (isNaN(x));
+
+// console.log(x, x*x)
+
+// console.log("12" == 12)
+
+// console.log("12" * "12")
+
+// let x = 123456789001234
+// let y = 12345678901234123
+// console.log({x})
+
+
+// let g = 0.1+0.2
+// console.log(+g.toFixed(2))
+// console.log(g
+
+
+let username = 'muharrem';
+
+console.log('merhaba', userName)
+
