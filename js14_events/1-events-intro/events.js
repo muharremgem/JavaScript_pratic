4b5f4ea4fe173c45f51709eccb06570a2ed19c47
muharrem gem <muharremgem28@gmail.com>
2022-09-12 22:06:28 +0300

    commit

--- js14_events/1-events-intro/events.js
+++ js14_events/1-events-intro/events.js
@@ -12,6 +12,7 @@ const h1 = document.querySelector("#header h1");
 h1.onmouseover = function(){
     h1.style.color = "red";
     h1.style.fontWeight = "300"
+
 };
 
 h1.onmouseout = () => {
@@ -28,12 +29,43 @@ h1.addEventListener('click', () => {
 
 document.getElementById("btn").addEventListener('click', function(){
     const input = document.querySelector("#input")
-    if(!input.value){
-        alert("please enter a item")
-    }else{
-        alert(`${input.calur} entered`)
-    }
-    input.value = "";
+    // if(!input.value){
+    //     alert("please enter a item")
+    // }else{
+    //     alert(`${input.calur} entered`)
+    // }
+    // input.value = "";
     
 });
 
+const list = document.querySelectorAll(".list")
+
+list.forEach((li)=> {
+    li.style.transition = "all 0.8s"
+    li.style.lineHeight = "2rem"
+    li.onmouseover = () => {
+        li.style.fontSize = "2rem";
+        li.style.transform = "translateX(10px)"
+    };
+
+    li.onmouseout = () => {
+        li.style.fontSize = "1rem";
+        li.style.transform = "translateX(-10px)"
+    };
+})
+
+//****   Example 4 */
+
+window.onload = function(){
+    document.querySelector("#input").focus();
+    
+}
+
+
+
+
+const print = () => {
+    console.log("Starting");
+}
+
+print()
