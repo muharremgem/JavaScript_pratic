15cca1bfde2bb55c43ba1b38f89a34493c9af6f2
muharrem gem <muharremgem28@gmail.com>
2022-09-20 19:38:14 +0300

    ımmediate function

--- Nesne tabanlı/oop/script.js
+++ Nesne tabanlı/oop/script.js
@@ -127,3 +127,34 @@
 
 
 // console.log(aylin.calculateAge());
+
+//* Immediate Function
+
+// function welcome(){
+//     var days = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+//     var today = new Date();
+//     var msg = "Welcome. Today is "+days[today.getDay()];
+//     return msg;
+
+// }
+
+// console.log(welcome())
+
+// (function(){
+
+// }());
+
+// (function(){
+
+// })();        //*  <=
+//* İKİ FARKLI ŞEKİLDE EN SON KOYDUGUMUZ PARANTEZLERİ KOYUP FONKSİYONLARI OLUSTURABILIRIZ.**/
+
+
+(function(name) {
+    var days = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+    var today = new Date();
+    var msg = ' Welcome ' +name+ ' Today is ' +days[today.getDay()];
+
+    console.log(msg);
+
+}("Muharrem"));
